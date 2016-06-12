2f970883a67aabbcdbe28a64c961ca42f17fe02d
Adam Smith <adam@imaginate.life>
2016-06-12 05:06:03 -0700

    Added trimListValueEnd parse method

---
+++ src/parse/list/one-line/trim-value-end.js
@@ -0,0 +1,50 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+var same = require('../../../help/vitals').same;
+
+var isSpace = require('../../../help/is-whitespace');
+
+var COMMA = ',';
+var CLOSE = ']';
+
+/**
+ * @param {string} line
+ * @param {number} i
+ * @return {number}
+ */
+module.exports = function trimListValueEnd(line, i) {
+
+  /** @type {string} */
+  var char;
+  /** @type {number} */
+  var len;
+
+  len = line.length;
+  while (++i < len) {
+
+    char = line[i];
+
+    if ( isSpace(char) ) continue;
+
+    if ( same(char, COMMA) ) break;
+    if ( same(char, CLOSE) ) break;
+
+    return 0;
+  }
+
+  return i;
+};
