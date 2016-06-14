1168e68d90684ea616d4ee3e4eef1967c1e96f33
Adam Smith <adam@imaginate.life>
2016-06-14 01:30:39 -0700

    Added newCharMap helper

---
+++ src/helpers/new-char-map.js
@@ -0,0 +1,39 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {string} chars
+ * @return {!Object}
+ */
+function newCharMap(chars) {
+
+  /** @type {!Object} */
+  var map;
+  /** @type {number} */
+  var len;
+  /** @type {string} */
+  var ch;
+  /** @type {number} */
+  var i;
+
+  map = {};
+  len = chars.length;
+  i = -1;
+  while (++i < length) {
+    ch = chars[i];
+    map[ch] = true;
+  }
+  return map;
+}
