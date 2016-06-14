fcf2d63b319c3492df5d87cab40881d7650f0a6e
Adam Smith <adam@imaginate.life>
2016-06-14 03:23:06 -0700

    Added parse skip methods

---
+++ src/parse/skip.js
@@ -0,0 +1,35 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @type {function}
+ */
+function skipWhitespace() {
+  while ( isWhitespace(DATA[$i]) ) ++$i;
+}
+
+/**
+ * @private
+ * @type {function}
+ */
+function skipComment() {
+  while (++$i < LEN) {
+    if ( isLineBreak(DATA[$i]) ) {
+      ++$line;
+      ++$i;
+      break;
+    }
+  }
+}
