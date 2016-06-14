c803fac3f3ebbe0269cc2848c8ef09f5919678e6
Adam Smith <adam@imaginate.life>
2016-06-14 14:27:05 -0700

    Added isComma helper

---
+++ src/helpers/is-comma.js
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {string} ch - one character
+ * @return {boolean}
+ */
+var isComma = (function _build_isComma() {
+
+  /**
+   * @private
+   * @type {string}
+   * @const
+   */
+  var COMMA = ',';
+
+  /**
+   * @param {string} ch
+   * @return {boolean}
+   */
+  return function isComma(ch) {
+    return same(ch, COMMA);
+  };
+})();
