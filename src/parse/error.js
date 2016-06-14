54d9a741c78fac47b93d364a93421c9ba7790840
Adam Smith <adam@imaginate.life>
2016-06-14 03:23:40 -0700

    Added parse error methods

---
+++ src/parse/error.js
@@ -0,0 +1,24 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {string} msg
+ * @return {string}
+ */
+function err(msg) {
+  return FILE
+    ? fuse.string(msg, ' at line `', $line, '` in file `', FILE, '`')
+    : fuse.string(msg, ' at line `', $line, '`');
+}
