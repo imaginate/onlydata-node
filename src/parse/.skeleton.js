5e64c01c3272e64a252aa9a9f61cc762b60f87ed
Adam Smith <adam@imaginate.life>
2016-06-14 03:22:03 -0700

    Added parse skeleton

---
+++ src/parse/.skeleton.js
@@ -0,0 +1,33 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {!Object} config
+ * @param {string} data
+ * @param {string=} file
+ * @return {!Object}
+ */
+var parse = (function _build_parse() {
+
+  // INSERT ./vars.js
+  // INSERT ./init.js
+  // INSERT ./skip.js
+  // INSERT ./error.js
+  // INSERT ./parse.js
+  // INSERT ./parse-key.js
+  // INSERT ./parse-value.js
+
+  return init;
+})();
