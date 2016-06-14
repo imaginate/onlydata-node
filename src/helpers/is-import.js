34d71be8e8013cf85b53574c6ee311b22dd0f947
Adam Smith <adam@imaginate.life>
2016-06-14 15:58:55 -0700

    Added isImport helper

---
+++ src/helpers/is-import.js
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {string} val
+ * @return {boolean}
+ */
+var isImport = (function _build_isImport() {
+
+  /**
+   * @private
+   * @type {!RegExp}
+   * @const
+   */
+  var KEY = /^import$/i;
+
+  /**
+   * @param {string} val
+   * @return {boolean}
+   */
+  return function isImport(val) {
+    return has(val, KEY);
+  };
+})();
