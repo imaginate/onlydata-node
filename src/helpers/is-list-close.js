1851eadeb4138dcd1207fe70f0ac7e19febd1b85
Adam Smith <adam@imaginate.life>
2016-06-14 14:26:09 -0700

    Added isListClose helper

---
+++ src/helpers/is-list-close.js
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {string} ch - one character
+ * @return {boolean}
+ */
+var isListClose = (function _build_isListClose() {
+
+  /**
+   * @private
+   * @type {string}
+   * @const
+   */
+  var CLOSE = ']';
+
+  /**
+   * @param {string} ch
+   * @return {boolean}
+   */
+  return function isListClose(ch) {
+    return same(ch, CLOSE);
+  };
+})();
