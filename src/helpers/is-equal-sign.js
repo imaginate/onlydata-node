9b38f8ed1e06c96ecdcca4d8931e6ec89f13aa95
Adam Smith <adam@imaginate.life>
2016-06-14 02:51:45 -0700

    Added isEqualSign helper

---
+++ src/helpers/is-equal-sign.js
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {string} ch - one character
+ * @return {boolean}
+ */
+var isEqualSign = (function _build_isEqualSign() {
+
+  /**
+   * @private
+   * @type {string}
+   * @const
+   */
+  var EQ = '=';
+
+  /**
+   * @param {string} ch
+   * @return {boolean}
+   */
+  return function isEqualSign(ch) {
+    return same(ch, EQ);
+  };
+})();
