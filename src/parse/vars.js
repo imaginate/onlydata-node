2f7b72f9e75eb52362de6126c3fc06aa833f0a62
Adam Smith <adam@imaginate.life>
2016-06-14 03:22:31 -0700

    Added parse vars

---
+++ src/parse/vars.js
@@ -0,0 +1,77 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @type {!Object}
+ * @const
+ */
+var CONF;
+
+/**
+ * @private
+ * @type {string}
+ * @const
+ */
+var DATA;
+
+/**
+ * @private
+ * @type {string}
+ * @const
+ */
+var FILE;
+
+/**
+ * @private
+ * @type {number}
+ * @const
+ */
+var LEN;
+
+/**
+ * @private
+ * @type {number}
+ */
+var $line;
+
+/**
+ * @private
+ * @type {string}
+ */
+var $char;
+
+/**
+ * @private
+ * @type {string}
+ */
+var $key;
+
+/**
+ * @private
+ * @type {*}
+ */
+var $val;
+
+/**
+ * @public
+ * @type {!Object}
+ */
+var $map;
+
+/**
+ * @private
+ * @type {number}
+ */
+var $i;
