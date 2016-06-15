40706191478572d9eccab00fc21093989081610c
Adam Smith <adam@imaginate.life>
2016-06-14 19:36:59 -0700

    Added trimEndWhitespace helper

---
+++ src/helpers/trim-end-whitespace.js
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {string} content
+ * @return {string}
+ */
+var trimEndWhitespace = (function _build_trimEndWhitespace() {
+
+  /**
+   * @private
+   * @type {!RegExp}
+   * @const
+   */
+  var SPACE = /[ \t\v]+$/;
+
+  /**
+   * @param {string} content
+   * @return {string}
+   */
+  return function trimEndWhitespace(content) {
+    return cut(content, SPACE);
+  };
+})();
