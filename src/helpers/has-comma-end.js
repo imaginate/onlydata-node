ce4c56ae992d4219a6337f997eaf213cae13dea5
Adam Smith <adam@imaginate.life>
2016-06-16 17:42:35 -0700

    Added hasCommaEnd helper

---
+++ src/helpers/has-comma-end.js
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {string} str
+ * @return {boolean}
+ */
+var hasCommaEnd = (function _build_hasCommaEnd() {
+
+  /**
+   * @private
+   * @type {!RegExp}
+   * @const
+   */
+  var END = /,$/;
+
+  /**
+   * @param {string} str
+   * @return {boolean}
+   */
+  return function hasCommaEnd(str) {
+    return has(str, END);
+  };
+})();
