ca3755af18e8b1efc02e2888d432f256a0cbfa2a
Adam Smith <adam@imaginate.life>
2016-06-17 23:56:26 -0700

    Added node path lib ref

--- src/.skeleton.js
+++ src/.skeleton.js
@@ -32,6 +32,8 @@ var to     = vitals.to;
 var until  = vitals.until;
 
 var PATH = require('path');
+var getDirpath  = PATH.dirname;
+var resolvePath = PATH.resolve;
 
 // INSERT ./helpers/*.js
 // INSERT ./config/*.js
