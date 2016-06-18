ddc23aca436c00d79eff234fe7e900248bf909d9
Adam Smith <adam@imaginate.life>
2016-06-17 22:50:12 -0700

    Added isAtSign helper

---
+++ src/helpers/is-at-sign.js
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {string} ch - one character
+ * @return {boolean}
+ */
+var isAtSign = (function _build_isAtSign() {
+
+  /**
+   * @private
+   * @type {string}
+   * @const
+   */
+  var SIGN = '@';
+
+  /**
+   * @param {string} ch
+   * @return {boolean}
+   */
+  return function isAtSign(ch) {
+    return same(ch, SIGN);
+  };
+})();
