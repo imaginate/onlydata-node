84b2c316bc021e7680a6d4eaec3e87f43dd1c1ec
Adam Smith <adam@imaginate.life>
2016-06-20 16:45:13 -0700

    Added isNumber helper

---
+++ src/helpers/is-number.js
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0-beta.1
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {string} val
+ * @return {boolean}
+ */
+var isNumber = (function _build_isNumber() {
+
+  /**
+   * @private
+   * @type {!RegExp}
+   * @const
+   */
+  var NUM = /[+-]?[0-9][0-9_,]*(?:\.[0-9][0-9_]*)?(?:[eE][+-]?[0-9][0-9_,]*)?/;
+
+  /**
+   * @param {string} val
+   * @return {boolean}
+   */
+  return function isNumber(val) {
+    return has(val, NUM);
+  };
+})();
