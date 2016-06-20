e36aeffeb63e21782fb2bf1121226de3bbc9c253
Adam Smith <adam@imaginate.life>
2016-06-20 16:32:12 -0700

    Added cutNumHelpers helper

---
+++ src/helpers/cut-num-helpers.js
@@ -0,0 +1,37 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0-beta.1
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @private
+ * @param {string} num
+ * @return {string}
+ */
+var cutNumHelpers = (function _build_cutNumHelpers() {
+
+  /**
+   * @private
+   * @type {!RegExp}
+   * @const
+   */
+  var REGEX = /[,_]/g;
+
+  /**
+   * @param {string} num
+   * @return {string}
+   */
+  return function cutNumHelpers(num) {
+    REGEX.lastIndex = 0;
+    return cut(num, REGEX);
+  };
+})();
