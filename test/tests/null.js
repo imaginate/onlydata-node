ba2c973b20d83d67d90520707e43224c1e50c9c1
Adam Smith <adam@imaginate.life>
2016-06-20 17:31:43 -0700

    Added null tests

---
+++ test/tests/null.js
@@ -0,0 +1,37 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0-beta.2
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+suite('null tests', function() {
+
+  test('lower-case', function() {
+    var content  = 'nil1 = null \n';
+        content += 'nil2 = nil    ';
+    var map = onlydata(content);
+    assert( map.nil1 === null );
+    assert( map.nil2 === null );
+  });
+
+  test('upper-case', function() {
+    var content  = 'nil1 = NULL \n';
+        content += 'nil2 = Null \n';
+        content += 'nil3 = NIL  \n';
+        content += 'nil4 = Nil    ';
+    var map = onlydata(content);
+    assert( map.nil1 === null );
+    assert( map.nil2 === null );
+    assert( map.nil3 === null );
+    assert( map.nil4 === null );
+  });
+
+});
