70a9ff72db13a86a97cf398c1ce3b17aaa3fb7bb
Adam Smith <adam@imaginate.life>
2016-06-29 13:04:15 -0700

    Rebuilt tests init to add build action

--- test/init.js
+++ test/init.js
@@ -23,16 +23,62 @@ var get  = vitals.get;
 var cd = process.chdir;
 var log = require('log-ocd')();
 var Mocha = require('mocha');
-var resolvePath = require('path').resolve;
+var resolve = require('path').resolve;
 
 log.error.setConfig({
   'throw': false,
   'exit':  true
 });
 
-require('./setup');
-cd(__dirname);
+buildDist();
+setTests();
 runTests();
+
+/**
+ * @type {function}
+ */
+function buildDist() {
+
+  /** @type {function} */
+  var build;
+  /** @type {string} */
+  var path;
+
+  path = resolve(__dirname, '../act/build.js');
+  build = require(path).method;
+  try {
+    build();
+  }
+  catch (err) {
+    err.name = err.name || 'Error';
+    err.name = fuse('Internal `build` ', err.name);
+    log.error(err);
+  }
+}
+
+/**
+ * @type {function}
+ */
+function setTests() {
+
+  /** @type {string} */
+  var base;
+  /** @type {string} */
+  var path;
+
+  base = __dirname;
+
+  cd(base);
+
+  path = resolve(base, 'setup/helpers.js');
+  require(path);
+
+  path = resolve(base, 'setup/interface.js');
+  require(path);
+
+  path = resolve(base, 'setup/reporters.js');
+  require(path);
+}
 
 /**
  * @type {function}
@@ -44,7 +90,7 @@ function runTests() {
   /** @type {!Array} */
   var tests;
   /** @type {string} */
-  var dir;
+  var path;
 
   log.debug('Starting `onlydata` tests');
 
@@ -52,9 +98,10 @@ function runTests() {
   mocha.reporter('specky');
   mocha.ui('onlydata');
 
-  dir = resolvePath('tests');
-  tests = get.filepaths(dir, {
-    basepath: true,
+  path = resolve(__dirname, 'tests');
+  tests = get.filepaths(path, {
+    basepath:  true,
+    recursive: true,
     validExts: 'js'
   });
   each(tests, function(file) {
@@ -63,7 +110,7 @@ function runTests() {
 
   try {
     mocha.run(function() {
-      log.pass('Finished `onlydata` tests');
+      log.pass('Completed `onlydata` tests');
     });
   }
   catch (err) {
