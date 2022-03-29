09a525fe67305fcd02eacdbd4178ba7a4e081c18
Adam Smith <imagineadamsmith@gmail.com>
2022-03-28 23:31:07 -0700

    Fixed missed copyright and links

--- src/.frame.js
+++ src/.frame.js
@@ -5,8 +5,8 @@
  * @version 1.0.4
  * @see [OnlyData](http://onlydata.tech)
  *
- * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
- * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ * @author Adam Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ * @copyright 2022 Adam A Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
  *
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
